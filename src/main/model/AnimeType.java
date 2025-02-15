50fdf3e4e085986bf7d62d9ef19e180eeb7873a7
Pu Gong <pgong03@students.cs.ubc.ca>
2025-02-14 19:33:05 -0800

    Documentation for AnimeType and AnimeStatus

--- src/main/model/AnimeType.java
+++ src/main/model/AnimeType.java
@@ -1,5 +1,14 @@
 package model;
 
+//represents the different Anime type can be choose by users
+//when they create, search animes.
 public enum AnimeType {
-    
+    Action,
+    School,
+    Comedy,
+    Fantasy,
+    Romance,
+    Tragic,
+    Adventure
+
 }
--- src/main/model/WatchStatus.java
+++ src/main/model/WatchStatus.java
@@ -1,5 +1,7 @@
 package model;
 
+//represents a enumeration of Watch Status of the anime,
+//users would be use them when create, updating the animes.
 public enum WatchStatus {
     Watching,
     Completed,
