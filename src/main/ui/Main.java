1b469572ba180d8b9069d0fa41b5797117c70843
Pu Gong <pgong03@students.cs.ubc.ca>
2025-02-16 14:40:05 -0800

    Delete unused exception

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,11 +1,10 @@
 package ui;
 
-import exception.NumberException;
 //EFFECTS: Entries for the Anime List Manager Application
 
 public class Main {
     //stub
-    public static void main(String[] args) throws NumberException {
+    public static void main(String[] args) {
         new AnimeListConsoleUI();
     }
 }
