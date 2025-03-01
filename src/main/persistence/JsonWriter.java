d11b7b96d6fba67f1367f4f253e922432e9a1750
Pu Gong <pgong03@students.cs.ubc.ca>
2025-03-01 14:58:45 -0800

    Implementation for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -5,39 +5,45 @@ import java.io.PrintWriter;
 
 import org.json.JSONObject;
 
-// Represents a writer that writes JSON representation of AnimeList to file
+import model.AnimeList;
+
+// Represents a writer that writes JSON representation of Anime List to file
 public class JsonWriter {
+    private PrintWriter writer;
+    private String filePath;
+    private static final int TAB = 4;
 
     // EFFECTS: constructs writer to write to destination file
-    // MODIFIES: this
     public JsonWriter(String filepath) {
-        //stub
+        this.filePath = filepath;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        //stub
+        writer = new PrintWriter(filePath);
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of json object to file
-    public void write(JSONObject json) {
-       //stub
+    public void write(AnimeList list) {
+        JSONObject json = list.toJson();
+        saveToFile(json.toString(TAB)); 
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        //stub
+        writer.close();
     }
 
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-       //stub
+        writer.print(json);
     }
+
 
 }
