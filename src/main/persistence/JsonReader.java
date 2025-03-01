af22a41674b310d347b88ee977cae4656e9b0292
Pu Gong <pgong03@students.cs.ubc.ca>
2025-03-01 15:08:54 -0800

    Implementation for JsonReader class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -19,37 +19,61 @@ import model.WatchStatus;
 // Represents a reader that reads Anime List from JSON data stored in file
 public class JsonReader {
 
+    private String source;
 
-    public JsonReader() {
-        //stub
+    public JsonReader(String source) {
+        this.source = source;
     }
 
     // EFFECTS: reads Anime List from file and returns it;
     // throws IOException if an error occurs reading data from file
     public AnimeList read() throws IOException {
-    return null;
-        
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseAnimeList(jsonObject);
+
     }
 
     // EFFECTS: reads source file as string and returns it
-    private String readFile() throws IOException {
-        //stub
-        return null;
+    private String readFile(String source) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+        Files.lines(Paths.get(source), StandardCharsets.UTF_8)
+                .forEach(line -> contentBuilder.append(line));
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses Animelist from JSON object and returns it
-    private AnimeList parseAnimeList() {
-        return null;
+    private AnimeList parseAnimeList(JSONObject jsonObject) {
+        AnimeList list = new AnimeList();
+        parseAnimes(jsonObject, list);
+        return list;
     }
 
     // MODIFIES: list
     // EFFECTS: parses thingies from JSON object and adds them to anime list
-    private void parseAnimes() {
-        //stub
+    private void parseAnimes(JSONObject jsonObject, AnimeList list) {
+        JSONArray animesArray = jsonObject.getJSONArray("animes");
+        for (Object obj : animesArray) {
+            JSONObject animeJson = (JSONObject) obj;
+            parseAnime(animeJson, list);
+        }
     }
 
-    private void parseAnime() {
-        //stub
+    private void parseAnime(JSONObject animeJson, AnimeList list) {
+        String name = animeJson.getString("name");
+        JSONArray typesArray = animeJson.getJSONArray("types");
+        List<AnimeType> types = new ArrayList<>();
+
+        for (Object t : typesArray) {
+            String typeStr = (String) t;
+            types.add(AnimeType.valueOf(typeStr));
+        }
+
+        YearMonth releaseYM = YearMonth.parse(animeJson.getString("releaseYearMonth"));
+        WatchStatus status = WatchStatus.valueOf(animeJson.getString("watchStatus"));
+
+        Anime anime = new Anime(name, types, releaseYM, status);
+        list.addAnime(anime);
     }
 
 }
